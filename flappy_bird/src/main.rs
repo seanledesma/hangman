d6b088d363a843e6b6aa909b54a4fd266c0c351a
Sean Ledesma <87875153+seanledesma@users.noreply.github.com>
2023-04-13 00:10:45 -0600

    added stuff

--- flappy_bird/src/main.rs
+++ flappy_bird/src/main.rs
@@ -1,8 +1,9 @@
 fn main() {
 
-    
+    let mut v1: Vec<i32> = Vec::new(); //learning how vectors work
+    let mut v2 = vec![1, 2, 3];
 
-    for i in 0..5 {
+    for i in v2 { //learning how for loops work
        println!("*");
     }
 }
---
+++ flappy_bird/target/debug/deps/flappy_bird-a19f6c5b897bbc89.d
@@ -0,0 +1,5 @@
+/home/sean/Projects/programming_projects/rust_projects/flappy_bird/target/debug/deps/flappy_bird-a19f6c5b897bbc89: src/main.rs
+
+/home/sean/Projects/programming_projects/rust_projects/flappy_bird/target/debug/deps/flappy_bird-a19f6c5b897bbc89.d: src/main.rs
+
+src/main.rs:
